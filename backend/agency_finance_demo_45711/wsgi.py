1d06814f5d1d3784b03bbb98e86835e621607f4e
Crowdbotics <team@crowdbotics.com>
2024-01-30 23:36:12 +0000

    Initial commit

---
+++ backend/agency_finance_demo_45711/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for agency_finance_demo_45711 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "agency_finance_demo_45711.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "agency-finance-demo-45711",
+  url: "https://agency-finance-demo-45711.botics.co",
+  api: "https://agency-finance-demo-45711.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
